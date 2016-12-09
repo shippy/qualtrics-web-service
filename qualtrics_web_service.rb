cd4c74a877b2001894a1ee1b2216a83ca15a604b
Simon Podhajsky <simon.podhajsky@gmail.com>
2016-12-08 19:32:01 -0500

    Add test for maximum Qualtrics URI length

--- qualtrics_web_service.rb
+++ qualtrics_web_service.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 require 'sinatra/config_file'
 require 'pry' if development?
 
+# Test that Qualtrics recognizes URI query format for web service return values
 get '/' do
   returnVals = { things: :have, changed: :for, me: :but, thats: :okay}
   URI.encode_www_form(returnVals)
+end
+
+# Check the maximum length of a URI that Qualtrics will store
+get '/:values' do
+  "length=" + params[:values].to_s.length
 end
